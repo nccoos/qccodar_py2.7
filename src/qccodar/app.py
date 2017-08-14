8dd57e95a9a85f4a1073929648e613af6b25943c
Sara Haines <sarahaines@unc.edu>
2017-08-14 20:04:41 +0000

    added auto() and watchdog monitor

--- src/qccodar/app.py
+++ src/qccodar/app.py
@@ -24,17 +24,54 @@ from .codarutils import *
 
 __version__ = get_distribution("qccodar").version
 
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class Watcher:
+
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self, fulldatadir):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, fulldatadir, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print "Exiting qccodar auto-mode ..."
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'created':
+            # Take any action here when a file is first created.
+            print "Received created event - %s." % event.src_path
+
+        elif event.event_type == 'modified':
+            # Taken any action here when a file is modified.
+            print "Received modified event - %s." % event.src_path
+
+
+def auto(datadir, pattern):
+    """ Auto mode runs qc when new files generated in datadir until  """
+    w = Watcher()
+    fulldatadir = os.path.join(datadir, 'RadialMetric', pattern)
+    w.run(fulldatadir)
 
 def manual(datadir, pattern):
     """ Manual mode runs qc on all files in datadir """
 
-    fulldatadir = os.path.join(datadir, 'RadialMetric', pattern)
-
-    if not os.path.isdir(fulldatadir):
-        print "Error: qccodar manual --datadir %s --pattern %s" % (datadir, pattern)
-        print "Directory does not exist: %s " % fulldatadir
-        return
-    
     # get file listing of datadir
     fns = recursive_glob(os.path.join(datadir, 'RadialMetric', pattern), 'RDL*.ruv')
 
@@ -53,13 +90,11 @@ def manual(datadir, pattern):
         do_qc(datadir, fn, pattern)
 
     # get file list of RadialShorts
+    # depending on system and desired time span for merge, change the target time for file search
     fns = recursive_glob(os.path.join(datadir, 'RadialShorts_qcd', pattern), 'RDL*00.ruv')
 
     print 'Merging RadialShorts_qcd to Radials_qcd: ...'
     outdir = os.path.join(datadir, 'Radials_qcd', pattern)
-
-    if not os.path.isdir(outdir):
-        os.makedirs(outdir)
 
     # run LLUVMerger for each
     for fullfn in fns:
@@ -75,12 +110,36 @@ def main():
     # print arguments
 
     datadir, pattern = arguments['--datadir'], arguments['--pattern']
+    if arguments['manual']:
+        runarg = 'manual'
+    elif arguments['auto']:
+        runarg = 'auto'
+    else:
+        runarg = ''
 
+    indatadir = os.path.join(datadir, 'RadialMetric', pattern)
+    if not os.path.isdir(indatadir):
+        print "Error: qccodar %s --datadir %s --pattern %s" % (runarg, datadir, pattern)
+        print "Directory does not exist: %s " % indatadir
+        return
+
+    outdir1 = os.path.join(datadir, 'RadialShorts_qcd', pattern)
+    if not os.path.exists(outdir1):
+        os.makedirs(outdir1)
+
+    outdir2 = os.path.join(datadir, 'Radials_qcd', pattern)
+    if not os.path.isdir(outdir2):
+        os.makedirs(outdir2)
+ 
     if arguments['manual']:
         manual(datadir, pattern)
         return
 
     # create watchdog to monitor datadir
+    if arguments['auto']:
+        auto(datadir, pattern)
+        print '... stopped'
+        return
 
 if __name__ == "__main__":
     main()
