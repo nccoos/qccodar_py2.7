615eff16460e6e0d45bd17e22caf16ee846b6bd6
Sara Haines <sarahaines@unc.edu>
2017-08-15 13:47:03 +0000

    now initialize Handler(datadir, pattern)

--- src/qccodar/app.py
+++ src/qccodar/app.py
@@ -19,58 +19,17 @@ import glob
 
 from pkg_resources import get_distribution
 
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
 from .qcutils import *
 from .codarutils import *
 
 __version__ = get_distribution("qccodar").version
 
-import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-class Watcher:
-
-    def __init__(self):
-        self.observer = Observer()
-
-    def run(self, fulldatadir):
-        event_handler = Handler()
-        self.observer.schedule(event_handler, fulldatadir, recursive=True)
-        self.observer.start()
-        try:
-            while True:
-                time.sleep(5)
-        except:
-            self.observer.stop()
-            print "Exiting qccodar auto-mode ..."
-
-        self.observer.join()
-
-
-class Handler(FileSystemEventHandler):
-
-    @staticmethod
-    def on_any_event(event):
-        if event.is_directory:
-            return None
-
-        elif event.event_type == 'created':
-            # Take any action here when a file is first created.
-            print "Received created event - %s." % event.src_path
-
-        elif event.event_type == 'modified':
-            # Taken any action here when a file is modified.
-            print "Received modified event - %s." % event.src_path
-
-
-def auto(datadir, pattern):
-    """ Auto mode runs qc when new files generated in datadir until  """
-    w = Watcher()
-    fulldatadir = os.path.join(datadir, 'RadialMetric', pattern)
-    w.run(fulldatadir)
-
 def manual(datadir, pattern):
-    """ Manual mode runs qc on all files in datadir """
+    """ Manual mode runs qc and merge on all files in datadir """
 
     # get file listing of datadir
     fns = recursive_glob(os.path.join(datadir, 'RadialMetric', pattern), 'RDL*.ruv')
@@ -100,6 +59,56 @@ def manual(datadir, pattern):
     for fullfn in fns:
         print fullfn
         run_LLUVMerger(fullfn, outdir, pattern)
+
+def auto(datadir, pattern, fullfn):
+    """ Auto mode runs qc and merge when new files generated in path being watched """
+    
+    # get file listing of datadir
+    fns = recursive_glob(os.path.join(datadir, 'RadialMetric', pattern), 'RDL*.ruv')
+
+    fn = os.path.basename(fullfn)
+    do_qc(datadir, fn, pattern)
+
+
+class Watcher:
+
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self, datadir, pattern):
+        event_handler = Handler(datadir, pattern)
+        fulldatadir = os.path.join(datadir, 'RadialMetric', pattern)
+        self.observer.schedule(event_handler, fulldatadir, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print '... qccodar auto-mode stopped'
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    def __init__(self, datadir, pattern):
+        self.datadir = datadir
+        self.pattern = pattern
+
+    def on_any_event(self, event):
+        if event.is_directory:
+            return None
+        elif event.event_type == 'created':
+            # Take any action here when a file is first created.
+            print "Received created event - %s." % event.src_path
+            print 'datadir = %s' % self.datadir
+            print 'pattern = %s' % self.pattern
+            auto(self.datadir, self.pattern, event.src_path)
+            
+        elif event.event_type == 'modified':
+            # Taken any action here when a file is modified.
+            print "Received modified event - %s." % event.src_path
 
 
 def main():
@@ -137,8 +146,9 @@ def main():
 
     # create watchdog to monitor datadir
     if arguments['auto']:
-        auto(datadir, pattern)
-        print '... stopped'
+        w = Watcher()
+        print 'Starting qccodar auto-mode (Press cntrl-C to exit)...'
+        w.run(datadir, pattern)
         return
 
 if __name__ == "__main__":
